ff6af048af460b1c3b294611ecd9586ccd48c826
willian <willian.araujo1994@gmail.com>
2024-06-25 18:44:34 -0300

    feat: render products
    - adjusted base url name in env.ts
    - removed '/api' at baseUrl
    - new type Product
    - imported products in home page

--- devstore/src/env.ts
+++ devstore/src/env.ts
@@ -1,15 +1,19 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string().url(),
-});
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
 
 const parsedEnv = envSchema.safeParse(process.env);
 
 if (!parsedEnv.success) {
   //flatten transforma os erros em um formato mais legível
-  console.error('Invalid environment variables:', parsedEnv.error.flatten().fieldErrors);
-  throw new Error('Invalid environment variables');
+  console.error(
+    'Invalid environment variables:',
+    parsedEnv.error.flatten().fieldErrors
+  )
+
+  throw new Error('Invalid environment variables.')
 }
 
-export const env = parsedEnv.data;
+export const env = parsedEnv.data
