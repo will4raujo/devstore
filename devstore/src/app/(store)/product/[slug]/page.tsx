271e1609ff9e5c3f44d3d7d860a832a5af22fc15
willian <willian.araujo1994@gmail.com>
2024-06-26 13:41:16 -0300

    feat: generateStaticParams
    - generating products highliths in build of application

--- devstore/src/app/(store)/product/[slug]/page.tsx
+++ devstore/src/app/(store)/product/[slug]/page.tsx
@@ -34,6 +34,18 @@ export async function generateMetadata({
   }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+
+  const products = await response.json()
+
+  return products.map((product: Product) => ({
+    params: {
+      slug: product.slug,
+    },
+  }))
+}
+
 export default async function ProductPage({params}: ProductProps) {
 
   const product = await getProduct(params.slug)
